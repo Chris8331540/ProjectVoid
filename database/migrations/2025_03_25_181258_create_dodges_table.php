d3f8603b8c1666ba02186f9d88d430ef888dc14e
Chris <mendozachristopher833@gmail.com>
2025-03-26 12:24:29 +0100

    Modificación de migraciones de Dodges

--- database/migrations/2025_03_25_181258_create_dodges_table.php
+++ database/migrations/2025_03_25_181258_create_dodges_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Agent;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('dodges', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("info");
+            $table->integer("order");
+            $table->string("image");
+
+            $table->foreignIdFor(Agent::class);
             $table->timestamps();
         });
     }
--- database/migrations/2025_03_25_181507_create_dodge_other_properties_table.php
+++ database/migrations/2025_03_25_181507_create_dodge_other_properties_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\DodgeMultiplier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('dodge_other_properties', function (Blueprint $table) {
             $table->id();
+            $table->integer("lvl");
+            $table->string("multiplier");
+
+            $table->foreignIdFor(DodgeMultiplier::class);
             $table->timestamps();
         });
     }
