c4b0890ffa4f3e4d5666e1f2d42edbbca2a6b133
Chris <mendozachristopher833@gmail.com>
2025-03-26 12:19:48 +0100

    Modificación de migraciones de Basics

--- database/migrations/2025_03_25_175524_create_basic_other_properties_table.php
+++ database/migrations/2025_03_25_175524_create_basic_other_properties_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\BasicMultiplier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('basic_other_properties', function (Blueprint $table) {
             $table->id();
+            $table->integer("lvl");
+            $table->string("multiplier");
+
+            $table->foreignIdFor(BasicMultiplier::class);
             $table->timestamps();
         });
     }
