f3200a87a5176466cc9cb3c2eebef4cb34bb57f4
Chris <mendozachristopher833@gmail.com>
2025-03-26 12:28:54 +0100

    Modificación de migraciones Assists

--- database/migrations/2025_03_26_102533_create_assist_multipliers_table.php
+++ database/migrations/2025_03_26_102533_create_assist_multipliers_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Assist;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('assist_multipliers', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("multiplier_type");
+            $table->integer("order");
+
+            $table->foreignIdFor(Assist::class);
             $table->timestamps();
         });
     }
