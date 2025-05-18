c45333d23a600b332623256122f71e1367172f12
Chris <mendozachristopher833@gmail.com>
2025-05-18 18:41:24 +0200

    añadiendo plantillas para crear pj

--- resources/js/pages/agents/Create.vue
+++ resources/js/pages/agents/Create.vue
@@ -40,20 +40,69 @@ function insertAgentDataTemplate() {
     agentDataJson.value = JSON.stringify(agentData, null, 4);
 }
 
-function insertCoreSkillTemplate(){
-    const coreSkillData = {
-        "coreSkill":{
+//tomar en cuenta que los valores de coreSkillMultipliers son un array por cada valor (6 niveles) es decir, que es posible tener de 1 a X valores, y opr lo tanto arrays
+//de la misma forma los coreSkillAdditions, X valores y 6 niveles, por lo que de 1 a X arrays
+function insertCoreSkillTemplate() {
+    const coreSkillData = [{
+        "coreSkill": {
             "name": "Core Passive Graceful Andante",
             "info": "During the Idyllic Cadenza state...",
-            "order" : 1,
-            "image" : ""
+            "order": 1,
+            "image": ""
         },
-        "coreSkillMultipliers": ["24", "26", "28", "30", "32", "35"],
-        "coreSkillAttributes":["atk"],
-        "coreSkillAdditions":["0", "25", "25", "50", "50", "75"]
-    }
+        "coreSkillMultipliers": [["24", "26", "28", "30", "32", "35"]],
+        "coreSkillAttributes": ["atk"],
+        "coreSkillAdditions": [["0", "25", "25", "50", "50", "75"]]
+    }]
 
     coreSkillDataJson.value = JSON.stringify(coreSkillData, null, 4);
+}
+
+function insertBasicSkillTemplate() {
+    const basicSkillData = [
+        {
+            "basics": [{
+                "name": "\"Capriccio\"",
+                "info": "Press {basic} to activate:<br>Perform up to three attacks forward, dealing Ether DMG.<br>During the 3rd-hit, hold {basic} to charge, drawing enemies in and expanding the attack range for a stronger attack. After using other skills, hold {basic} to initiate the 3rd-hit directly.",
+                "image": "",
+            }],
+            "basicMultipliers": [
+                {
+                    "name": "1st-Hit",
+                    "multiplier_type": "atk",
+                    "dmgs_dazes": [
+                        [["43.80%", "41.30%"], ["59.10%", "56.40%"], ["120.90%", "114.80%"], ["270.70%", "219.40%"]],
+                        [["47.80%", "43.20%"], ["64.50%", "59.00%"], ["131.90%", "120.10%"], ["295.40%", "229.40%"]],
+                        [["51.80%", "45.10%"], ["69.90%", "61.60%"], ["142.90%", "125.40%"], ["320.10%", "239.40%"]],
+                        [["55.80%", "47.00%"], ["75.30%", "64.20%"], ["153.90%", "130.70%"], ["344.80%", "249.40%"]],
+                        [["59.80%", "48.90%"], ["80.70%", "66.80%"], ["164.90%", "136.00%"], ["369.50%", "259.40%"]],
+                        [["63.80%", "50.80%"], ["86.10%", "69.40%"], ["175.90%", "141.30%"], ["394.20%", "269.40%"]],
+                        [["67.80%", "52.70%"], ["91.50%", "72.00%"], ["186.90%", "146.60%"], ["418.90%", "279.40%"]],
+                        [["71.80%", "54.60%"], ["96.90%", "74.60%"], ["197.90%", "151.90%"], ["443.60%", "289.40%"]],
+                        [["75.80%", "56.50%"], ["102.30%", "77.20%"], ["208.90%", "157.20%"], ["468.30%", "299.40%"]],
+                        [["79.80%", "58.40%"], ["107.70%", "79.80%"], ["219.90%", "162.50%"], ["493.00%", "309.40%"]],
+                        [["83.80%", "60.30%"], ["113.10%", "82.40%"], ["230.90%", "517.70%"], ["493.00%", "319.40%"]],
+                        [["87.80%", "62.20%"], ["118.50%", "85.00%"], ["241.90%", "173.10%"], ["542.40%", "329.40%"]],
+                        [["87.80%", "62.20%"], ["118.50%", "85.00%"], ["241.90%", "173.10%"], ["542.40%", "329.40%"]],
+                        [["87.80%", "62.20%"], ["118.50%", "85.00%"], ["241.90%", "173.10%"], ["542.40%", "329.40%"]],
+                        [["87.80%", "62.20%"], ["118.50%", "85.00%"], ["241.90%", "173.10%"], ["542.40%", "329.40%"]],
+                        [["87.80%", "62.20%"], ["118.50%", "85.00%"], ["241.90%", "173.10%"], ["542.40%", "329.40%"]],
+                    ]
+                },
+                {
+                    "name": "2nd-Hit",
+                    "multiplier_type": "atk",
+                },
+                {
+                    "name": "1st-Hit",
+                    "multiplier_type": "atk",
+                }
+            ],
+
+
+        }
+    ]
+
 }
 </script>
 
